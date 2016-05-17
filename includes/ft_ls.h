013263775992aaf6a03451d834bfd3537add2d7b
Simon GAUDIN <sgaudin@e2r8p15.42.fr>
2016-05-17 11:56:34 +0200

    beggining

---
+++ includes/ft_ls.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ls.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sgaudin <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/05/17 11:09:05 by sgaudin           #+#    #+#             */
+/*   Updated: 2016/05/17 11:15:56 by sgaudin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LS_H
+# define FT_LS_H
+
+# include <dirent.h>
+# include <sys/stat.h>
+# include <uuid/uuid.h>
+# include <sys/xattr.h>
+# include <time.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+#endif
